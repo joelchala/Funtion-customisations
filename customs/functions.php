d3c30d465cb5187726c9cddb0fc972053b086ce9
joelchala <71480801+joelchala@users.noreply.github.com>
2022-03-01 12:47:25 -0400

    Create functions.php

---
+++ customs/functions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Functions.php
+ *
+ * @package  Funtion_customisations
+ * @author   WooThemes, colaborations: joelchala
+ * @since    1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * functions.php
+ * Add PHP snippets here
+ */
